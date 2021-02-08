11f20d584fb5b09be6a9e5833ccb314051da8138
oloc <oloc@pulse>
2021-02-08 12:39:03 +0100

    Ajout du déplacement horizontal pour la classe joueur

--- player.py
+++ player.py
@@ -16,3 +16,9 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = 400
         self.rect.y = 500
+
+    def move_left(self):
+        self.rect.x -= self.velocity
+
+    def move_right(self):
+        self.rect.x += self.velocity
