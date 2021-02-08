23d1bdda33541f59f3df484c5524dccb307c8c83
oloc <oloc@pulse>
2021-02-08 11:55:53 +0100

    Ajout de la génération de la fenêtre.

---
+++ main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pygame
+
+pygame.init()
+
+# Générer la fenêtre du jeu
+pygame.display.set_caption("Comet Fall Game")
+pygame.display.set_mode(size=(500, 300))
