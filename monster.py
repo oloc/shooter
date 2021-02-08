8773985900b0040473952a877351eba500806567
oloc <oloc@pulse>
2021-02-08 19:28:30 +0100

    Ajout de la barre de vie des monstres

--- monster.py
+++ monster.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from pygame import image
+from pygame import draw, image
 from pygame.sprite import Sprite
+from pygame.surface import Surface
 
 
 class Monster(Sprite):
@@ -18,6 +19,11 @@ class Monster(Sprite):
         self.rect.x = 1000
         self.rect.y = 540
         self.velocity = 5
+
+    def update_health_bar(self, surface: Surface):
+        bar_color = (111, 210, 46)
+        bar_position = [self.rect.x + 10, self.rect.y - 20, self.health, 5]
+        draw.rect(surface, bar_color, bar_position)
 
     def forward(self):
         if not self.game.check_collision(self, self.game.all_players):
