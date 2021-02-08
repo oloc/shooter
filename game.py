649b73f3bd41c9138ca06654bfa471928ad5bea6
oloc <oloc@pulse>
2021-02-08 18:50:57 +0100

    Ajout du monster dans la game

--- game.py
+++ game.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from pygame.sprite import Group
+
+from monster import Monster
 from player import Player
 
 
@@ -8,4 +11,11 @@ class Game:
 
     def __init__(self):
         self.player = Player()
+        self.all_monsters = Group()
         self.pressed = dict()
+
+        self.spawn_monster()
+
+    def spawn_monster(self):
+        monster = Monster()
+        self.all_monsters.add(monster)
