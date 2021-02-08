33b32f70d4afb278c34a20423dd959f6b447f8b4
oloc <oloc@pulse>
2021-02-08 13:49:44 +0100

    Ajout de la rotation des projectiles

--- projectile.py
+++ projectile.py
@@ -12,14 +12,22 @@ class Projectile(pygame.sprite.Sprite):
         self.velocity = 5
         self.image = pygame.image.load('assets/projectile.png')
         self.image = pygame.transform.scale(self.image, (50, 50))
+        self.origin_image = self.image
+        self.angle = 0
         self.rect = self.image.get_rect()
         self.rect.x = player.rect.x + 120
         self.rect.y = player.rect.y + 80
 
     def move(self) -> None:
         self.rect.x += self.velocity
+        self.rotate()
         if self.rect.x > 1080:
             self.remove()
 
     def remove(self) -> None:
         self.player.all_projectiles.remove(self)
+
+    def rotate(self) -> None:
+        self.angle += 12
+        self.image = pygame.transform.rotozoom(self.origin_image, self.angle, 1)
+        self.rect = self.image.get_rect(center=self.rect.center)
