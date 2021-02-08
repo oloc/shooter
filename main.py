3b115b3de22af54b1dd3a07d3fded75307d7ece5
oloc <oloc@pulse>
2021-02-08 12:00:27 +0100

    Ajout de la boucle de maintien du jeu avec la fermeture de fenêtre

--- main.py
+++ main.py
@@ -8,3 +8,15 @@ pygame.init()
 # Générer la fenêtre du jeu
 pygame.display.set_caption("Comet Fall Game")
 pygame.display.set_mode(size=(500, 300))
+
+game_is_running = True
+
+# Boucle tant que le jeu est en cours
+while game_is_running:
+
+    # Boucle sur les événements
+    for event in pygame.event.get():
+        # événement de fermeture de fenêtre
+        if event.type == pygame.QUIT:
+            game_is_running = False
+            pygame.quit()
