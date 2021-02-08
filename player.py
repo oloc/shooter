1b32840222ff92db0ea1cb4ab799761deda5a7b8
oloc <oloc@pulse>
2021-02-08 12:13:03 +0100

    Création de Player

---
+++ player.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pygame
+
+
+class Player(pygame.sprite.Sprite):
+
+    def __init__(self):
+        super().__init__()
+        self.health = 100
+        self.max_health = 100
+        self.attack = 10
+        self.velocity = 5
