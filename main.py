9c10ae0d2d3df8bdd3003b1f854dbf60d39cc414
oloc <oloc@pulse>
2021-02-08 12:06:27 +0100

    Ajout de l'arrière plan.

--- main.py
+++ main.py
@@ -7,12 +7,19 @@ pygame.init()
 
 # Générer la fenêtre du jeu
 pygame.display.set_caption("Comet Fall Game")
-pygame.display.set_mode(size=(1080, 720))
+screen = pygame.display.set_mode(size=(1080, 720))
+
+background = pygame.image.load('assets/bg.jpg')
 
 game_is_running = True
 
 # Boucle tant que le jeu est en cours
 while game_is_running:
+
+    # Application de l'arrière plan sur la surface
+    screen.blit(background, (0, -200))
+    # mise à jour de l'écran
+    pygame.display.flip()
 
     # Boucle sur les événements
     for event in pygame.event.get():
