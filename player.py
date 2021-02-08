29b40808a3db51e454c567ff96fa33d39f2b9041
oloc <oloc@pulse>
2021-02-08 13:08:56 +0100

    Ajout du lancer de projectiles pour le joueur

--- player.py
+++ player.py
@@ -3,6 +3,8 @@
 
 import pygame
 
+from projectile import Projectile
+
 
 class Player(pygame.sprite.Sprite):
 
@@ -12,10 +14,15 @@ class Player(pygame.sprite.Sprite):
         self.max_health = 100
         self.attack = 10
         self.velocity = 5
+        self.all_projectiles = pygame.sprite.Group()
         self.image = pygame.image.load('assets/player.png')
         self.rect = self.image.get_rect()
         self.rect.x = 400
         self.rect.y = 500
+
+    def launch_projectile(self) -> None:
+        # Instanciation du projectile dans le groupe
+        self.all_projectiles.add(Projectile())
 
     def move_left(self):
         self.rect.x -= self.velocity
