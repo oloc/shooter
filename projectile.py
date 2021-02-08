8e59fff577d63d86a91ba61a3578faac94f16ba5
oloc <oloc@pulse>
2021-02-08 13:34:44 +0100

    Amélioration empirique du point de départ du projectile

--- projectile.py
+++ projectile.py
@@ -12,5 +12,5 @@ class Projectile(pygame.sprite.Sprite):
         self.image = pygame.image.load('assets/projectile.png')
         self.image = pygame.transform.scale(self.image, (50, 50))
         self.rect = self.image.get_rect()
-        self.rect.x = player.rect.x
-        self.rect.y = player.rect.y
+        self.rect.x = player.rect.x + 120
+        self.rect.y = player.rect.y + 80
