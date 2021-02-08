47bee69b1819370a1b6b8486c32fd557eaaf9584
oloc <oloc@pulse>
2021-02-08 19:40:41 +0100

    Ajout d'une arrière barre de vie

--- monster.py
+++ monster.py
@@ -21,8 +21,11 @@ class Monster(Sprite):
         self.velocity = 5
 
     def update_health_bar(self, surface: Surface):
+        back_bar_color = (60, 63, 60)
         bar_color = (111, 210, 46)
+        back_bar_position = [self.rect.x + 10, self.rect.y - 20, self.max_health, 5]
         bar_position = [self.rect.x + 10, self.rect.y - 20, self.health, 5]
+        draw.rect(surface, back_bar_color, back_bar_position)
         draw.rect(surface, bar_color, bar_position)
 
     def forward(self):
