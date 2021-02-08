faedeca5ffb261ea56e3fad3ddc052dd7ca23dd4
oloc <oloc@pulse>
2021-02-08 19:49:40 +0100

    Ajout des dégâts des projectiles

--- projectile.py
+++ projectile.py
@@ -21,8 +21,9 @@ class Projectile(pygame.sprite.Sprite):
     def move(self) -> None:
         self.rect.x += self.velocity
         self.rotate()
-        if self.player.game.check_collision(self, self.player.game.all_monsters):
+        for monster in self.player.game.check_collision(self, self.player.game.all_monsters):
             self.remove()
+            monster.damage(self.player.attack)
         if self.rect.x > 1080:
             self.remove()
 
