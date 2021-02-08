4614c114a6f8476bc457bf2da7ecbb0780621a53
oloc <oloc@pulse>
2021-02-08 13:27:20 +0100

    Amélioration taille du projectile

--- projectile.py
+++ projectile.py
@@ -10,4 +10,5 @@ class Projectile(pygame.sprite.Sprite):
         super().__init__()
         self.velocity = 5
         self.image = pygame.image.load('assets/projectile.png')
+        self.image = pygame.transform.scale(self.image, (50, 50))
         self.rect = self.image.get_rect()
