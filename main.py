57a7a3a057dfde73a3898975425299d3b7647561
oloc <oloc@pulse>
2021-02-08 12:25:12 +0100

    Ajout du joueur sur la surface

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 
 import pygame
 
+from player import Player
+
 pygame.init()
 
 # Générer la fenêtre du jeu
@@ -10,6 +12,7 @@ pygame.display.set_caption("Comet Fall Game")
 screen = pygame.display.set_mode(size=(1080, 720))
 
 background = pygame.image.load('assets/bg.jpg')
+player = Player()
 
 game_is_running = True
 
@@ -18,6 +21,8 @@ while game_is_running:
 
     # Application de l'arrière plan sur la surface
     screen.blit(background, (0, -200))
+    # Application du joueur sur la surface
+    screen.blit(player.image, player.rect)
     # mise à jour de l'écran
     pygame.display.flip()
 
