26a56eafc7ffa5731d0970c033105326163336d7
oloc <oloc@pulse>
2021-02-09 15:32:16 +0100

    Amélioration du typage de monster

--- monster.py
+++ monster.py
@@ -10,7 +10,7 @@ from pygame.surface import Surface
 
 class Monster(Sprite):
 
-    def __init__(self, game):
+    def __init__(self, game) -> None:
         super().__init__()
         self.game = game
         self.health = 100
@@ -22,22 +22,22 @@ class Monster(Sprite):
         self.rect.y = 540
         self.velocity = random.randint(1, 3)
 
-    def damage(self, amount):
+    def damage(self, amount: int) -> None:
         self.health -= amount
         if self.health <= 0:
             self.recycle()
 
-    def update_health_bar(self, surface: Surface):
+    def update_health_bar(self, surface: Surface) -> None:
         draw.rect(surface, (60, 63, 60), [self.rect.x + 10, self.rect.y - 20, self.max_health, 5])
         draw.rect(surface, (111, 210, 46), [self.rect.x + 10, self.rect.y - 20, self.health, 5])
 
-    def forward(self):
+    def forward(self) -> None:
         if not self.game.check_collision(self, self.game.all_players):
             self.rect.x -= self.velocity
         else:
             self.game.player.damage(self.attack)
 
-    def recycle(self):
+    def recycle(self) -> None:
         self.rect.x = 1000 + random.randint(0, 300)
         self.health = self.max_health
         self.velocity = random.randint(1, 3)
