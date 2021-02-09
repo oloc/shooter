f7357c15e157ee1c77fbb54a964bac41d121bffa
oloc <oloc@pulse>
2021-02-09 15:52:08 +0100

    Amélioration de la barre de comète

--- comet_event.py
+++ comet_event.py
@@ -9,10 +9,13 @@ class CometFallEvent:
 
     def __init__(self) -> None:
         self.percent = int()
+        self.percent_speed = 5
 
-    def add_percent(self, amount: int = 1) -> None:
-        self.percent += amount
+    def add_percent(self) -> None:
+        self.percent += self.percent_speed / 100
 
     def update_bar(self, surface: Surface) -> None:
+        self.add_percent()
         draw.rect(surface, (0, 0, 0), [0, surface.get_height() - 20, surface.get_width(), 10])
-        draw.rect(surface, (187, 11, 11), [0, surface.get_height() - 20, (surface.get_width() / 100) * self.percent, 10])
+        draw.rect(surface, (187, 11, 11),
+                  [0, surface.get_height() - 20, (surface.get_width() / 100) * self.percent, 10])
