e820cfacf895d441172fa340c8ad26306b32d6ab
oloc <oloc@pulse>
2021-02-09 15:57:10 +0100

    Ajout de la mécanique de pluie de comètes

--- comet_event.py
+++ comet_event.py
@@ -14,8 +14,19 @@ class CometFallEvent:
     def add_percent(self) -> None:
         self.percent += self.percent_speed / 100
 
+    def attempt_fall(self) -> None:
+        if self.is_full_loaded():
+            self.reset_percent()
+
+    def is_full_loaded(self) -> bool:
+        return self.percent >= 100
+
+    def reset_percent(self):
+        self.percent = 0
+
     def update_bar(self, surface: Surface) -> None:
         self.add_percent()
+        self.attempt_fall()
         draw.rect(surface, (0, 0, 0), [0, surface.get_height() - 20, surface.get_width(), 10])
         draw.rect(surface, (187, 11, 11),
                   [0, surface.get_height() - 20, (surface.get_width() / 100) * self.percent, 10])
