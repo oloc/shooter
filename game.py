4f4fe1cdf80efefa692694b5a02e0642d4786d88
oloc <oloc@pulse>
2021-02-09 12:41:13 +0100

    Ajout de la mise à jour de la partie

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from pygame import K_LEFT, K_RIGHT
 from pygame.sprite import Group, Sprite, collide_mask, spritecollide
 
 from monster import Monster
@@ -10,6 +11,7 @@ from player import Player
 class Game:
 
     def __init__(self):
+        self.is_playing = False
         self.player = Player(self)
         self.all_players = Group()
         self.all_players.add(self.player)
@@ -25,3 +27,29 @@ class Game:
     def spawn_monster(self):
         monster = Monster(self)
         self.all_monsters.add(monster)
+
+    def update(self, screen):
+        # Application du joueur sur la surface
+        screen.blit(self.player.image, self.player.rect)
+
+        # Application de la barre de vie du joueur
+        self.player.update_health_bar(screen)
+
+        # Déplacement des projectiles
+        for projectile in self.player.all_projectiles:
+            projectile.move()
+        # Déplacement des montres
+        for monster in self.all_monsters:
+            monster.forward()
+            monster.update_health_bar(screen)
+
+        # Application des projectiles sur la surface
+        self.player.all_projectiles.draw(screen)
+        # Application des monstres sur la surface
+        self.all_monsters.draw(screen)
+
+        # Déplacement du joueur
+        if self.pressed.get(K_RIGHT) and self.player.rect.x + self.player.rect.width < screen.get_width():
+            self.player.move_right()
+        elif self.pressed.get(K_LEFT) and self.player.rect.x > 0:
+            self.player.move_left()
--- main.py
+++ main.py
@@ -20,30 +20,10 @@ while game_is_running:
 
     # Application de l'arrière plan sur la surface
     screen.blit(background, (0, -200))
-    # Application du joueur sur la surface
-    screen.blit(game.player.image, game.player.rect)
 
-    # Application de la barre de vie du joueur
-    game.player.update_health_bar(screen)
-
-    # Déplacement des projectiles
-    for projectile in game.player.all_projectiles:
-        projectile.move()
-    # Déplacement des montres
-    for monster in game.all_monsters:
-        monster.forward()
-        monster.update_health_bar(screen)
-
-    # Application des projectiles sur la surface
-    game.player.all_projectiles.draw(screen)
-    # Application des monstres sur la surface
-    game.all_monsters.draw(screen)
-
-    # Déplacement du joueur
-    if game.pressed.get(pygame.K_RIGHT) and game.player.rect.x + game.player.rect.width < screen.get_width():
-        game.player.move_right()
-    elif game.pressed.get(pygame.K_LEFT) and game.player.rect.x > 0:
-        game.player.move_left()
+    # Vérification si le jeu a commencé
+    if game.is_playing:
+        game.update(screen)
 
     # mise à jour de l'écran
     pygame.display.flip()
