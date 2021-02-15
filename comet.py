578d9b4f65b61ffc136fc52501e2254a7f2fd0db
oloc <oloc@pulse>
2021-02-15 14:33:58 +0100

    Amélioration de la position de départ des comètes en aléatoire

--- comet.py
+++ comet.py
@@ -12,6 +12,8 @@ class Comet(sprite.Sprite):
         super().__init__()
         self.image = image.load('assets/comet.png')
         self.rect = self.image.get_rect()
+        self.rect.x = randint(20, 800)
+        self.rect.y = - randint(0, 800)
         self.velocity = randint(1, 3)
 
     def fall(self):
