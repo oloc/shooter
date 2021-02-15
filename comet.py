477e99e9a39b740269b9f90bb1de417c4b31e7c9
oloc <oloc@pulse>
2021-02-15 14:21:51 +0100

    Création de la comète

---
+++ comet.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from pygame import image, sprite
+
+
+class Comet(sprite.Sprite):
+
+    def __init__(self):
+        super().__init__()
+        self.image = image.load('assets/comet.png')
+        self.rect = self.image.get_rect()
